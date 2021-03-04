36094c8e0a3298a6a40b2d821f654f1cf7ec0a69
panarik <panarik@yandex.ru>
2021-03-04 21:20:29 +0300

    add solutions

--- 1.JavaSyntax/src/com/javarush/task/task03/task0313/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0313/Solution.java
@@ -1,12 +1,26 @@
 package com.javarush.task.task03.task0313;
 
-/* 
-Мама мыла раму
-*/
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+
+        int abc;
+        int acb;
+        int bca;
+        int cba;
+        int cab;
+        int bac;
+
+        String a = "Мама";
+        String b = "Мыла";
+        String c = "Раму";
+
+        System.out.println(a+b+c);
+        System.out.println(a+c+b);
+        System.out.println(b+c+a);
+        System.out.println(c+b+a);
+        System.out.println(c+a+b);
+        System.out.println(b+a+c);
 
     }
 }
