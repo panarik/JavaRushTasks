218356c0cfce7eac4f2d98f4cdb5ea1a2f99ada2
panarik <panarik@yandex.ru>
2021-03-08 22:16:55 +0300

    add solutions

---
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0324/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task03.task0324;
+
+/* 
+Меркантильные намерения
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+        String a = "Я хочу большую зарплату, и для этого изучаю Java";
+        System.out.println(a+"\n"+a+"\n"+a+"\n"+a+"\n"+a+"\n"+a+"\n"+a+"\n"+a+"\n"+a+"\n"+a);
+
+    }
+}
