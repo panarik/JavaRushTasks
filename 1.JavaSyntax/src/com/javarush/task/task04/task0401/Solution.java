ee50a1d9406925c0241f1bf1dd4c6ae1c02d5b3f
panarik <panarik@yandex.ru>
2021-04-21 22:45:04 +0300

    add solutions

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0401/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task04.task0401;
+
+/* 
+Мне не подходит этот возраст…
+*/
+
+public class Solution {
+    
+    static int age = 20;
+    
+    public static void main(String[] args) {
+        Person person = new Person();
+        System.out.println("Age is: " + person.age);
+        person.adjustAge(person.age);
+        System.out.println("Adjusted age is: " + age);
+    }
+
+    public static class Person {
+        public int age = 20;
+
+        public void adjustAge(int age) {
+            this.age = age + 20;
+            System.out.println("Age in adjustAge() is: " + this.age);
+        }
+    }
+}
