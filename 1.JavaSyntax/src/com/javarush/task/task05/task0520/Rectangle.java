cf4f8ec1bd48a13b5809f31db118460854c11285
panarik <panarik@yandex.ru>
2021-05-07 18:26:15 +0300

    add solutions

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0520/Rectangle.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task05.task0520;
+
+/* 
+Создать класс прямоугольник (Rectangle)
+*/
+
+
+public class Rectangle {
+
+    int top;
+    int left;
+    int width;
+    int height;
+
+    public Rectangle(int top) {
+    }
+
+    public Rectangle(int top, int left) {
+    }
+
+    public Rectangle(int top, int left, int width) {
+    }
+
+    public Rectangle(int top, int left, int width, int height) {
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
